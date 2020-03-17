b63abd0b52747100a9e4b56827ed66f00b7a043f
Loïc DURAND <loicdurand.ld@gmail.com>
2020-03-17 18:17:44 +0100

    some problems encountered with parcel-bundler. Replacing the generated bin folder by the original files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const //
-
-    { ldbundler } = require('./builder'),
-    { getOptions } = require('./options'),
-    options = getOptions(process.argv.slice(2));
-
-exports['ld-bundler'] = ldbundler(options);
